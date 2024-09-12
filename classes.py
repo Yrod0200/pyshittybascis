4229a4405456d0da3512ce27bfca4a03816f1444
Yrod0200 <yurisantarosarodrigues3@gmail.com>
2024-09-11 22:01:53 -0300

    now more f*ing beautiful (idk if github will ban me if i swear)

---
+++ classes.py
@@ -0,0 +1,23 @@
+from sys import stdout
+import io
+
+
+messages = ["Hello", ", ", "World", "!"]
+printer = io.StringIO()
+
+class HelloWord():
+    def __init__(self) -> None:
+        self.i = ""
+        pass
+    def loop_for_each_word_and_then_set_everything_on_io(self):
+        for message in messages:
+            self.i += message
+        printer.write(self.i)
+    def after_getting_everything_on_the_io_then_print_everything(self):
+        stdout.write(printer.getvalue())
+
+
+if __name__ == '__main__':
+    helloworld = HelloWord()
+    helloworld.loop_for_each_word_and_then_set_everything_on_io()
+    helloworld.after_getting_everything_on_the_io_then_print_everything()
